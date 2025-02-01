3db9b1ffcfac7bd8ea708cc99e04a46d10f5bf5b
Dylan Dayrit <157668404+dylndayrit@users.noreply.github.com>
2025-02-01 19:07:28 +0000

    delete previous random joke generator

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,28 +10,6 @@ st.write(
 )
 st.image("cat.webp")
 
-### Copilot 
-# List of jokes
-#jokes = [
-#    "Why don't scientists trust atoms? Because they make up everything!",
-#    "Why did the math book look sad? Because it had too many problems.",
-#    "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears!",
-#    "Why did the scarecrow win an award? Because he was outstanding in his field!",
-#    "What do you call fake spaghetti? An impasta!"
-#]
-
-# Function to get a random joke
-#def get_random_joke():
-#    return random.choice(jokes)
-
-# Streamlit app
-#st.title('Random Joke Generator')
-
-#if st.button('Tell me a joke!'):
-#    joke = get_random_joke()
-#    st.write(joke)
-# Fetch joke function (without caching)
-
 def fetch_joke():
     url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
     try:
